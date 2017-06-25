f881fc40c55de438affe1c60eb7aa20a919793e7
Dylan Awalt-Conley <dawaltconley@wesleyan.edu>
2017-06-25 19:17:37 -0400

    added autoprefixer support for gulp deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 var child = require('child_process');
+var autoprefixer = require('gulp-autoprefixer');
+var runSequence = require('run-sequence');
 
 function jekyllBuild(env = 'development') {
     env = 'JEKYLL_ENV=' + env + ' ';
     child.execSync(env + 'jekyll build', { stdio: 'inherit' });
 }
 
+gulp.task('prefix', function () {
+    return gulp.src('./_site/css/main.css')
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./_site/css'))
+});
+
 gulp.task('deploy', function () {
     jekyllBuild('production');
+    runSequence(['prefix']);
     return gulp.src('./_site/**/*')
         .pipe(ghPages());
 });
