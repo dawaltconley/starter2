86df25b22d244d847cccfe9a572cf56137a8a68a
Dylan Awalt-Conley <dawaltconley@wesleyan.edu>
2017-06-25 19:10:33 -0400

    made a function out of building jekyll from gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,13 @@ var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 var child = require('child_process');
 
+function jekyllBuild(env = 'development') {
+    env = 'JEKYLL_ENV=' + env + ' ';
+    child.execSync(env + 'jekyll build', { stdio: 'inherit' });
+}
+
 gulp.task('deploy', function () {
-    child.execSync('JEKYLL_ENV=production jekyll build');
+    jekyllBuild('production');
     return gulp.src('./_site/**/*')
         .pipe(ghPages());
 });
