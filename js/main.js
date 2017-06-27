417cfadca557f162760de8553491ad7db0fc1f76
Dylan Awalt-Conley <dawaltconley@wesleyan.edu>
2017-06-26 23:46:05 -0400

    removed unused javascript

--- js/main.js
+++ js/main.js
@@ -19,144 +19,12 @@
         return Array.prototype.slice.call(collection);
     }
 
-    const getTransitionTime = function (element, ...properties) {
-        let dur = 0;
-        const transition = window.getComputedStyle(element).getPropertyValue("transition").split(", ");
-        if (properties.length > 0) {
-            properties.forEach( function (property) {
-                transition.forEach( function (transitionSet) {
-                    let propertyMatch = new RegExp("(\\s|^)" + property + "(\\s|$)");
-                    if (transitionSet.search(propertyMatch) >= 0) {
-                        transitionSet.split(" ").forEach( function (value) {
-                            if (value.search(/\ds$/) >= 0) {
-                                const seconds = Number(value.replace("s", ""));
-                                dur += seconds;
-                            }
-                        });
-                    }
-                });
-            });
-        } else {
-            let durElement, durSet = [];
-            transition.forEach( function (transitionSet) {
-                durElement = 0;
-                transitionSet.split(" ").forEach( function (value) {
-                    if (value.search(/\ds$/) >= 0) {
-                        const seconds = Number(value.replace("s", ""));
-                        durElement += seconds;
-                    }
-                });
-                durSet.push(durElement);
-            });
-            dur = Math.max.apply(null, durSet);
-        }
-        return dur * 1000;
-    }
-
-    const scrollBottom = function (element) { //opposite of .scrollTop: measures dist between bottom of view and bottom of element
-        const elementBottom = element.scrollHeight;
-        const viewBottom = element.scrollTop + element.clientHeight;
-        return elementBottom - viewBottom;
-    }
-
-    const getParentBySelector = function (element, selector) {
-        let ancestor = element;
-        while (ancestor != document.body) {
-            ancestor = ancestor.parentElement;
-            if (Sizzle.matchesSelector(ancestor, selector)) {
-                return ancestor;
-            }
-        }
-        return null;
-    }
-
-    const getChildBySelector = function (element, selector) {
-        for (var i = 0; i < element.children.length; i++) {
-            const child = element.children[i];
-            if (Sizzle.matchesSelector(child, selector)) {
-                return child;
-            } else if (child.children.length) {
-                const childMatch = getChildBySelector(child, selector);
-                if (childMatch) {return childMatch;}
-            }
-        }
-        return null;
-    }
-
-    const getChildrenBySelector = function (element, selector) {
-        let matches = [];
-        for (var i = 0; i < element.children.length; i++) {
-            const child = element.children[i];
-            if (Sizzle.matchesSelector(child, selector)) {
-                matches.push(child);
-            }
-            if (child.children.length) {
-                const childMatch = getChildrenBySelector(child, selector);
-                if (childMatch) {
-                    childMatch.forEach( function (match) {
-                        matches.push(match);
-                    });
-                }
-            }
-        }
-        if (matches.length) {
-            return matches;
-        } else {
-            return null;
-        }
-    }
-
-    const clearClass = function (string) {
-        let className;
-        if (string.charAt(0) === ".") {
-            className = string.slice(1);
-        } else {
-            className = string;
-        }
-        const allOfClass = toArray(document.querySelectorAll("." + className));
-        allOfClass.forEach( function (element) {
-            element.classList.remove(className);
-        });
-    }
-
-    const contentWidth = function (element) {
-        const elementStyle = window.getComputedStyle(element);
-        const elementPadding = parseInt(elementStyle.getPropertyValue("padding-left")) + parseInt(elementStyle.getPropertyValue("padding-right"));
-        return element.clientWidth - elementPadding;
-    }
-
-    const contentHeight = function (element) {
-        const elementStyle = window.getComputedStyle(element);
-        const elementPadding = parseInt(elementStyle.getPropertyValue("padding-top")) + parseInt(elementStyle.getPropertyValue("padding-bottom"));
-        return element.clientHeight - elementPadding;
-    }
-
-    const parseBoolean = function (string) {
-        if (string == "true") {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
 /*
  * Scrolling
  */
 
     const pageScrollBehavior = window.getComputedStyle(page).getPropertyValue("scroll-behavior");
     const smoothLinks = toArray(document.querySelectorAll('[data-scroll="smooth"]'));
-
-    const getHash = function (element) { //should remove, not using
-        const elementClass = Object.prototype.toString.call(element);
-        if (elementClass == "[object SVGAElement]") {
-            const link = element.href.baseVal;
-            return link.slice(link.search("#"));
-        } else if (elementClass == "[object HTMLAnchorElement]") {
-            return element.hash;
-        } else {
-            return null;
-        }
-    }
 
     const smoothScrollToTop = function () {
         win.scroll({
@@ -172,7 +40,7 @@
     }
 
     const smoothScrollToLinkTarget = function (link) {
-        const hash = getHash(link);
+        const hash = link.hash;
         const target = document.querySelector(hash);
         window.history.pushState({ hasFocus: hash }, hash.slice(1), hash);
         smoothScrollTo(target);
