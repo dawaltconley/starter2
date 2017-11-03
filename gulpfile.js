848585d84e8e49f3f675f6388d060d0f2e8998df
Dylan Awalt-Conley <dawaltconley@wesleyan.edu>
2017-11-03 02:44:13 -0400

    removed github deploy gulp task and replaced with default build

--- gulpfile.js
+++ gulpfile.js
@@ -5,40 +5,31 @@ var autoprefixer = require("gulp-autoprefixer");
 var babel = require("gulp-babel");
 var concat = require("gulp-concat");
 var uglify = require("gulp-uglify");
-var runSequence = require("run-sequence");
 
 function jekyllBuild(env = "development") {
     env = "JEKYLL_ENV=" + env + " ";
     child.execSync(env + "jekyll build", { stdio: "inherit" });
 }
 
-gulp.task("prefix", function () {
+gulp.task("build", jekyllBuild.bind(null, "production"));
+
+gulp.task("prefix", ["build"], function () {
     return gulp.src("./_site/css/main.css")
         .pipe(autoprefixer())
         .pipe(gulp.dest("./_site/css"));
 });
 
-gulp.task("babel", function () {
+gulp.task("babel", ["build"], function () {
     return gulp.src("./_site/js/main.js")
         .pipe(babel())
         .pipe(gulp.dest("./_site/js"));
 });
 
-gulp.task("concat", function () {
+gulp.task("uglify", ["babel"], function () {
     return gulp.src(["./_site/js/*.polyfill.js", "./_site/js/main.js"])
         .pipe(concat("main.js"))
-        .pipe(gulp.dest("./_site/js"))
-});
-
-gulp.task("uglify", function () {
-    return gulp.src("./_site/js/*.js")
         .pipe(uglify())
         .pipe(gulp.dest("./_site/js"))
 });
 
-gulp.task("deploy", function () {
-    jekyllBuild("production");
-    runSequence(["prefix", "babel"], "concat", "uglify");
-    return gulp.src("./_site/**/*")
-        .pipe(ghPages());
-});
+gulp.task("default", ["build", "babel", "uglify"]);
