e8abe9b04042f0b5ccd0d6e48d0d4cb747148f03
Dylan Awalt-Conley <dawaltconley@wesleyan.edu>
2018-01-31 02:11:11 -0500

    added bg-image resize support, updated gulpfile to latest projects

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,94 @@
 var gulp = require("gulp");
-var child = require("child_process");
-var autoprefixer = require("gulp-autoprefixer");
-var babel = require("gulp-babel");
 var concat = require("gulp-concat");
 var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
 var clean = require("gulp-clean");
+var imageResize = require("gulp-image-resize");
+var imageMin = require("gulp-imagemin");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var pump = require("pump");
+var merge = require("merge-stream");
+var child = require("child_process");
+var YAML = require("js-yaml");
+var fs = require("fs");
 
 function jekyllBuild(env = "development") {
-    env = "JEKYLL_ENV=" + env + " ";
-    child.execSync(env + "jekyll build", { stdio: "inherit" });
+    var cmd = "JEKYLL_ENV=" + env + " jekyll build";
+    child.execSync(cmd, { stdio: "inherit" });
 }
 
 gulp.task("build", jekyllBuild.bind(null, "netlify"));
 
-gulp.task("prefix", ["build"], function () {
-    return gulp.src("./_site/css/main.css")
-        .pipe(autoprefixer())
-        .pipe(gulp.dest("./_site/css"));
+gulp.task("css", ["build"], function (cb) {
+    pump([
+        gulp.src("./_site/css/main.css"),
+        postcss([
+            autoprefixer()
+        ]),
+        gulp.dest("./_site/css")
+    ], cb);
 });
 
-gulp.task("babel", ["build"], function () {
-    return gulp.src("./_site/js/main.js")
-        .pipe(babel())
-        .pipe(gulp.dest("./_site/js"));
-});
-
-gulp.task("uglify", ["babel"], function () {
-    return gulp.src([
+gulp.task("js", ["build"], function (cb) {
+    pump([
+        gulp.src([
             "./_site/js/polyfills/*.js",
             "./_site/js/lib/*.js",
             "./_site/js/main.js",
             "!./_site/**/*.min.js",
             "!./_site/**/*.map"
-        ])
-        .pipe(concat("all.js"))
-        .pipe(uglify())
-        .pipe(gulp.dest("./_site/js"))
+        ]),
+        concat("all.js"),
+        uglify(),
+        gulp.dest("./_site/js")
+    ], cb);
 });
 
-gulp.task("clean-js", ["uglify"], function () {
-    return gulp.src([
+gulp.task("clean-js", ["js"], function (cb) {
+    pump([
+        gulp.src([
             "./_site/js/*",
             "!./_site/js/all.js"
-        ], { read: false })
-        .pipe(clean())
+        ], { read: false }),
+        clean()
+    ], cb);
 });
 
-gulp.task("default", ["build", "prefix", "babel", "uglify", "clean-js"]);
+gulp.task("image-min", ["build"], function (cb) {
+    pump([
+        gulp.src([
+            "./_site/assets/**/*",
+            "!./_site/assets/**/*.svg",
+            "!./_site/assets/test/*"
+        ]),
+        imageMin(),
+        gulp.dest("./_site/assets")
+    ], cb);
+});
+
+gulp.task("bg-images", ["image-min"], function () {
+    var imageBreakpoints = YAML.safeLoad(fs.readFileSync("_config.yml", "utf8"))["image_bp"];
+    var src = "./_site/assets/backgrounds/*";
+    var dest = "./_site/assets/backgrounds";
+    var merged = merge();
+    imageBreakpoints.forEach(function (bp) {
+        var stream = gulp.src(src)
+            .pipe(
+                imageResize({
+                    width: bp.x,
+                    height: bp.y,
+                    cover: true,
+                    upscale: false,
+                    filter: "Catrom",
+                    interlace: true
+                })
+            )
+            .pipe(rename({ suffix: "-" + bp.x + "x" + bp.y }))
+            .pipe(gulp.dest(dest));
+        merged.add(stream);
+    });
+    return merged.isEmpty() ? null : merged;
+});
+
+gulp.task("default", ["build", "css", "js", "clean-js", "image-min", "bg-images"]);
