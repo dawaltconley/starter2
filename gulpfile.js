2a6e237d1e2200c636bb2d4e5af3bb9b1b22f41d
Dylan Awalt-Conley <dawaltconley@wesleyan.edu>
2018-06-02 04:39:27 -0400

    had to move picturefill so it isn't deleted on build

--- gulpfile.js
+++ gulpfile.js
@@ -50,7 +50,7 @@ gulp.task("clean-js", ["js"], function (cb) {
         gulp.src([
             "./_site/js/*",
             "!./_site/js/all.js",
-            "!./_site/js/polyfills/picturefill.min.js"
+            "!./_site/js/picturefill.min.js"
         ], { read: false }),
         clean()
     ], cb);
