094a4d6691ceb9f6abf84e7254872ad0f0c754c4
Dylan Awalt-Conley <dawaltconley@wesleyan.edu>
2018-06-04 19:15:19 -0400

    check to see if google analytics installed

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@
     }
 
     jekyllEnv = "{{ jekyll.environment }}";
+    hasGoogleAnalytics = "{{ site.google_analytics }}";
 /*
  * General-purpose functions
  */
@@ -429,7 +430,7 @@
         addHideOnScrollListener();
     }
 
-    if (analyticsObjects.length > 0 && jekyllEnv == "gulp") {
+    if (analyticsObjects.length > 0 && hasGoogleAnalytics && jekyllEnv == "gulp") {
         analyticsObjects.forEach(function (object) {
             object.addListener()
         });
