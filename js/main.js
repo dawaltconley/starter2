1e4af4ecfd10a4615334171486459979c1f1dc25
Dylan Awalt-Conley <dawaltconley@wesleyan.edu>
2018-06-11 17:01:05 -0400

    added condition so links don't open twice

--- js/main.js
+++ js/main.js
@@ -346,11 +346,11 @@
             eventObj.send();
         } else {
             event.preventDefault();
+            var linkFollowed = false;
             function followLink() {
-                if (eventObj.element.target) {
+                if (!linkFollowed) {
+                    linkFollowed = true;
                     window.open(eventObj.element.href, eventObj.element.target);
-                } else {
-                    window.open(eventObj.element.href);
                 }
             }
             window.setTimeout(followLink, 1000);
