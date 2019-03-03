3ffa0162e53485d78494658a53ecac05887292d9
Dylan Awalt-Conley <dawaltconley@wesleyan.edu>
2019-03-03 17:38:17 -0500

    set pagePos to only add scroll calculations when offsetHeight is LESS than scrollHeight; this avoids breaking with a rounding error, which sometimes causes offsetHeight to be 1 greater than scrollHeight

--- js/main.js
+++ js/main.js
@@ -94,7 +94,7 @@
             left: cRect.left - pRect.left,
             right: cRect.right - pRect.left
         }
-        if (parent.offsetHeight != parent.scrollHeight) {
+        if (parent.offsetHeight < parent.scrollHeight) {
             rRect.top = rRect.top + parent.scrollTop;
             rRect.bottom = rRect.bottom + parent.scrollTop;
             rRect.left = rRect.left + parent.scrollLeft;
