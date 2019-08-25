93634b42afc8b27b8cd42bdd3050385966e4b999
Dylan Awalt-Conley <web@dylan.ac>
2019-08-25 19:04:29 -0400

    added option to hide template items, generally cleaned up render code & stopped returning children (which breaks)

--- admin/js/live-preview.js
+++ admin/js/live-preview.js
@@ -54,23 +54,33 @@ const DefaultTemplate = createClass({
     render: function () {
         return HTMLReactParser(this.state.html, {
             replace: ({ name, attribs, children }) => {
-                if (attribs) {
-                    let { 'data-preview-field': field, 'data-preview-widget': widget, 'data-preview-asset': asset, 'data-preview-date': date } = attribs
-                    if (field) {
-                        let value = this.props.entry.getIn(['data', ...field.split('.')]) || children
-                        if (date) value = dayjs(value).format(date)
-                        return h(name, attribs, value)
-                    } else if (widget) {
-                        let w = widget.split('.')
-                        if (w.length === 1) {
-                            return h(name, attribs, this.props.widgetFor(w[0]) || children)
-                        } else if (w.length === 2) {
-                            return h(name, attribs, this.props.widgetsFor(w[0]).getIn(['widgets', w[1]]) || children)
-                        }
+                if (!attribs) return
+                let { 'data-preview-field': field, 'data-preview-widget': widget, 'data-preview-asset': asset, 'data-preview-date': date, 'data-preview-hide': hide } = attribs
+                if (
+                    field
+                    && ( field = this.props.entry.getIn(['data', ...field.split('.')]) ) !== undefined
+                ) {
+                    if (date) field = dayjs(field).format(date)
+                    return h(name, attribs, field)
+                }
+                if (widget) {
+                    let w = widget.split('.')
+                    if (
+                        w.length === 1
+                            && ( widget = this.props.widgetFor(w[0]) ) !== undefined
+                        || w.length === 2
+                            && ( widget = this.props.widgetsFor(w[0]) ) !== undefined
+                            && ( widget = widget.getIn(['widgets', w[1]]) ) !== undefined
+                    ) {
+                        return h(name, attribs, widget)
                     }
-                    if (asset && name === 'img') {
-                        asset = this.props.entry.getIn(['data', ...asset.split('.')])
-                        asset = this.props.getAsset(asset)
+                }
+                if (
+                    asset
+                    && ( asset = this.props.entry.getIn(['data', ...asset.split('.')]) ) !== undefined
+                    && ( asset = this.props.getAsset(asset) ) !== undefined
+                ) {
+                    if (name === 'img') {
                         if (asset) {
                             return h(name, Object.assign(attribs, { src: asset.toString(), srcset: null }))
                         } else {
@@ -78,6 +88,7 @@ const DefaultTemplate = createClass({
                         }
                     }
                 }
+                if (hide) return h('Fragment')
             }
         })
     }
